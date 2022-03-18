1b76ded31e1f8548605ba434f044e280dffa4542
Stanley Balakai <sbalakai@gmail.com>
2022-03-18 04:39:01 -0400

    final clean up

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var bodyParser = require('body-parser')
 
 const PORT = process.env.PORT 
 
-// Initialize express application 
+
 const app = express();
 
 let session = require('express-session')
-// let passport = require('./helper/ppConfig')
+
 
 app.use(session({
     secret: process.env.secret,
@@ -20,29 +20,20 @@ app.use(session({
     cookie: {maxAge: 360000}
 }))
 
-// app.use(passport.initialize())
-// app.use(passport.session())
 
-// app.use(flash());
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// const expressLayouts = require("express-ejs-layouts")
 
-// look into views folder for a file named as layouts.ejs
-// app.use(expressLayouts);
-
-
-// Import Routes
 const frontpageRoute = require('./routes/frontpage')
 const mainRoute = require('./routes/main')
 
-// mount routes
+
 app.use('/', frontpageRoute)
 app.use('/main', mainRoute)
 
-// nodejs to look in a folder called views for all ejs files
+
 app.set("view engine", "ejs")
 
 
@@ -59,4 +50,3 @@ mongoose.connect(process.env.mongoDBURL,{
 app.listen(PORT, ()=> console.log(`App is running on ${PORT}`));
 
 
-// ./public/images/const-vs-let-vs-var (1).png
